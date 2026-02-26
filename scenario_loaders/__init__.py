f41acff1d0a1b6738a48c4c8e2095001e74d6dbc
harvest7777 <333harvest.moon333@gmail.com>
2026-02-25 16:42:21 -0800

    refactor: extract load_simulation_from_dict to load from a dict directly

--- scenario_loaders/__init__.py
+++ scenario_loaders/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 from .load_environment import load_environment
-from .load_simulation import load_simulation
+from .load_simulation import load_simulation, load_simulation_from_dict
 
-__all__ = ["load_environment", "load_simulation"]
+__all__ = ["load_environment", "load_simulation", "load_simulation_from_dict"]
