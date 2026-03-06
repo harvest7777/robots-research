70abd49059520d442f00d2fe41af672984fedc55
harvest7777 <333harvest.moon333@gmail.com>
2026-03-06 09:30:37 -0800

    chore: remove InMemoryAssignmentService, superseded by JsonAssignmentService

--- services/__init__.py
+++ services/__init__.py
@@ -1,13 +1,11 @@
 from services.base_assignment_service import BaseAssignmentService
 from services.base_simulation_state_service import BaseSimulationStateService, SimulationState, RobotStateSnapshot, TaskStateSnapshot
-from services.in_memory_assignment_service import InMemoryAssignmentService
 from services.json_assignment_service import JsonAssignmentService
 from services.json_simulation_state_service import JsonSimulationStateService
 
 __all__ = [
     "BaseAssignmentService",
     "BaseSimulationStateService",
-    "InMemoryAssignmentService",
     "JsonAssignmentService",
     "JsonSimulationStateService",
     "RobotStateSnapshot",
