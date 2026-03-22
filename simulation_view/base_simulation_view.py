7e307d42069ed0959788afb80d8455937ff8080a
harvest7777 <333harvest.moon333@gmail.com>
2026-03-21 21:04:47 -0700

    base view class

---
+++ simulation_view/base_simulation_view.py
@@ -0,0 +1,21 @@
+"""
+BaseSimulationView:
+
+Abstract class for the simulation view. Exposes rendering and shutdown
+methods. How the view implements this isn't our concern.
+"""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from simulation.domain.simulation_state import SimulationState
+
+class BaseAssignmentService(ABC):
+
+    @abstractmethod
+    def render(self, simulation_state: SimulationState) -> list[Assignment]:
+        """Render the current state of the simulation."""
+
+    def handle_exit(self):
+        """Gracefully call any cleanup/shutdown behaviors."""
+
