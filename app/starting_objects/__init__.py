a1cbd3dd6425c520b8bbaa6e110e9e1da86990f5
harvest7777 <333harvest.moon333@gmail.com>
2026-03-31 18:02:38 -0700

    chore: remove unused app in preference for simulation runner

--- app/starting_objects/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.starting_objects.environment import build_environment
-from app.starting_objects.robots import ROBOTS, ROBOT_STATES
-from app.starting_objects.tasks import TASKS, TASK_STATES
-
-__all__ = ["build_environment", "ROBOTS", "ROBOT_STATES", "TASKS", "TASK_STATES"]
